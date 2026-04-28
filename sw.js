02547545f2d68254e2b1703db9cdff012213684c
Fthku <drorop@gmail.com>
2026-04-29 00:21:48 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 const CACHE = 'fevercycle-v1';
 const ASSETS = [
-  '/fevercycle/',
-  '/fevercycle/index.html',
-  '/fevercycle/manifest.json',
-  '/fevercycle/icon-192.png',
-  '/fevercycle/icon-512.png'
+  '/fevertrack/',
+  '/fevertrack/index.html',
+  '/fevertrack/manifest.json',
+  '/fevertrack/icon-192.png',
+  '/fevertrack/icon-512.png'
 ];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
